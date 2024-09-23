8a99b844541c6a33f1f3990c0eb64cdd8d4501f5
ShadeOfMorr <shcheka.d@gmail.com>
2024-09-23 12:20:03 +0300

    Update hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -6,7 +6,9 @@
 #include <string>
 using namespace std;
 
-string name; 
+string name;  // Для чего?
+/* Параметр num не слишком очевиден (точнее его смысл), если не знать определения функции/
+ * Я бы убрал это ветвление и оставил просто печать. World можно и main'е передать просто */
 void hello(int num, string name){
 	if (num == 0) {
 		cout << "Hello, world!" << endl;
