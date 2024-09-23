1ef8357ac9cf373b628adfa90bf0dca0a3292588
ShadeOfMorr <shcheka.d@gmail.com>
2024-09-23 12:15:42 +0300

    Update header.h

--- header.h
+++ header.h
@@ -1,8 +1,12 @@
 //Lichkovaha Daniil
 //st129351@student.spbu.ru
 //problem_1
-
+/* Забыл написать include guards */
 #include <string>
+ 
+ /* На самом деле так делать нехорошо, потому что это загрязняет
+ * пространство имен и иногда провоцирует конфликты имет, поэтому 
+ * привыкай просто везде писать std:: */
 using namespace std;
 #include <iostream>
 
